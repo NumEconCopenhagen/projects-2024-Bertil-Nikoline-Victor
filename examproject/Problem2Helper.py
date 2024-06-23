811cbf442827a17cc4b0111baa721adc9a0c439c
TheRealVictor <victorvk98@gmail.com>
2024-06-23 15:14:12 +0200

    Trying to answer Q2.2

--- examproject/Problem2Helper.py
+++ examproject/Problem2Helper.py
@@ -5,6 +5,9 @@
 # Import random
 import random
 
+# Import math
+import math
+
 
 def error_term(par):
 
@@ -39,9 +42,6 @@ def average_realised_utility(par, career_choice):
     # Subtracting 1 for the career choice to be consistent with the mathematical formulation
     career_choice = career_choice - 1
 
-    # Set the seed for reproducibility
-    random.seed(1996)
-
     # Array for storing utilities
     utilities = []
 
@@ -62,88 +62,60 @@ def average_realised_utility(par, career_choice):
 # Question 2 #
 ##############
 
-def results_from_2(par):
+def simulate_for_individual(par, type):
 
-    range_of_individuals = range(0,par.N)
 
-    for i in range_of_individuals:
+    range_of_individuals = range(0, type)
+
+    # Store the results
+    best_expected_utility = -1 * math.inf
+    best_realized_utility = None
+    best_j = None
+
+    # J has been hardcoded to 3
+    J = 3
+
+    for j in range(J):
+
+        expected_utilities = []
+
+        for i in range_of_individuals:
         # Note that individual i starts from 0 in this case but 
         # actually represents individul 1 in the math stated in the question
 
-        # Number of friends person i has
-        Fi = par.F[i]
+            utility = simulate_utility(par, j)
 
-        # J has been hardcoded to 3
-        J = 3 
+            expected_utilities.append(utility)
 
-        # Array to store calculated info
-        career_track_info = []
+        utility_value = sum(expected_utilities) / len(expected_utilities)
 
-        highest_utility = -1 * math.inf
+        if utility_value > best_expected_utility:
+            best_expected_utility = utility_value
+            best_realized_utility = simulate_utility(par, j)
+            best_j = j + 1
 
-        for j in range(J):
-            
-            v_j = (j + 1)
+    info = (best_j, best_expected_utility, best_realized_utility)
 
-            # Error terms for further calculations
-            error_term_array = []
+    return info
 
-            for index in range(Fi):
-                error = error_term_friends(par)
-                error_term_array.append(error)
-            
-            utility_friends = v_j + (sum(error_term_array) / len(error_term_array))
 
-            utility_actual = v_j + error_term(par)
+def mean_expected_utility(array):
+    
+    utilities = [expected_utility for _, expected_utility, _ in array]
 
-            info = ((j + 1), utility_friends, utility_actual)
+    return sum(utilities) / len(utilities)
 
 
 
-            career_track_info.append(info)
-        
-        for x in career_track_info:
-            print(x)
+def mean_realized_utility(array):
+
+    utilities = [realized_utility for _, _, realized_utility in array]
+
+    return sum(utilities) / len(utilities)
 
 
-
-        # # Calculating error terms
-        # for index in range(J_times_Fi):
-        #     error_term = error_term_friends(par)
-        #     error_term_array.append(error_term)
-
-        # # This is an array of expected utility from each career path
-        # # where object 0 is career path 1
-        # expected_utility_from_carrers = []
-
-        # for j in range(J):
-            
-
-
-        # print(error_term_array)
-
-
-        # #[print(f'Individual {i} with F value {Fi}')]
-
-
-
-def error_term_friends(par):
-
-    # Define the mean and standard deviation
-    mean = 0
-    standard_deviation = (par.sigma**2)
-
-    return random.gauss(mean, standard_deviation)
-
-
-
-def average_utility_of_friends(par, Fi, j_career_choice):
-
-    for j in range(J):
-        print(f'j = {j}')
-
+def career_distribution(array):
 
     
 
-
-
+    return None
